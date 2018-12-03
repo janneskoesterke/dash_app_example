0642344759ca418ba10cd07250b917e7046acacb
janneskoesterke <43071230+janneskoesterke@users.noreply.github.com>
2018-12-03 09:57:31 +0100

    First push ever

---
+++ app.py
@@ -0,0 +1,152 @@
+
+# coding: utf-8
+
+# In[ ]:
+
+
+import dash
+import dash_core_components as dcc
+import dash_html_components as html
+import plotly.graph_objs as go
+import pandas as pd
+
+app = dash.Dash(__name__)
+server = app.server
+app.css.append_css({"external_url": "https://codepen.io/chriddyp/pen/bWLwgP.css"})
+
+
+df = pd.read_csv('nama_10_gdp_1_Data.csv')
+df = df[~df.GEO.str.contains('Euro')]
+df = df[~df.UNIT.str.contains('Chain')]
+df.head()
+
+### JOINED VERSION - TWO GRAPHS ###
+
+#create lists for drop-down menus
+available_geo = df['GEO'].unique()
+available_indicators = df['NA_ITEM'].unique()
+
+app.layout = html.Div([
+    #Graph 1 - Scatter Plot
+    html.Div([
+
+        html.Div([
+            dcc.Dropdown(
+                id='xaxis-column1',
+                options=[{'label': i, 'value': i} for i in available_indicators],
+                value='Gross domestic product at market prices'
+            )
+        ],
+        style={'width': '48%', 'display': 'inline-block'}),
+
+        html.Div([
+            dcc.Dropdown(
+                id='yaxis-column1',
+                options=[{'label': i, 'value': i} for i in available_indicators],
+                value='Wages and salaries'
+            )
+        ],style={'width': '48%', 'float': 'right', 'display': 'inline-block'})
+    ]),
+
+    dcc.Graph(id='indicator-graphic1'),
+
+    dcc.Slider(
+        id='year--slider',
+        min=df['TIME'].min(),
+        max=df['TIME'].max(),
+        value=df['TIME'].max(),
+        step=None,
+        marks={str(year): str(year) for year in df['TIME'].unique()}
+    ),
+
+###Graph 2 - Line Chart
+    html.Div([
+
+        html.Div([
+            dcc.Dropdown(
+                id='xaxis-column2',
+                options=[{'label': i, 'value': i} for i in available_geo],
+                value='France'
+            )
+        ],
+        style={'width': '48%', 'display': 'inline-block'}),
+
+        html.Div([
+            dcc.Dropdown(
+                id='yaxis-column2',
+                options=[{'label': i, 'value': i} for i in available_indicators],
+                value='Wages and salaries'
+            )
+        ],style={'width': '48%', 'float': 'right', 'display': 'inline-block'})
+    ]),
+
+    dcc.Graph(id='indicator-graphic2'),
+
+])
+
+###Graph 1 -Scatter Plot
+@app.callback(
+    dash.dependencies.Output('indicator-graphic1', 'figure'),
+    [dash.dependencies.Input('xaxis-column1', 'value'),
+     dash.dependencies.Input('yaxis-column1', 'value'),
+     dash.dependencies.Input('year--slider', 'value')])
+def update_graph(xaxis_column_name1, yaxis_column_name1,
+                 year_value):
+    dff = df[df['TIME'] == year_value]
+    
+    return {
+        'data': [go.Scatter(
+            x=dff[dff['NA_ITEM'] == xaxis_column_name1]['Value'],
+            y=dff[dff['NA_ITEM'] == yaxis_column_name1]['Value'],
+            text=dff[dff['NA_ITEM'] == yaxis_column_name1]['GEO'],
+            mode='markers',
+            marker={
+                'size': 15,
+                'opacity': 0.5,
+                'line': {'width': 0.5, 'color': 'white'}
+            }
+        )],
+        'layout': go.Layout(
+            xaxis={
+                'title': xaxis_column_name1,
+                'type': 'linear'
+            },
+            yaxis={
+                'title': yaxis_column_name1,
+                'type': 'linear'
+            },
+            margin={'l': 40, 'b': 40, 't': 10, 'r': 40},
+            hovermode='closest'
+        )
+    }
+
+###Graph 2 -Line Chart
+@app.callback(
+    dash.dependencies.Output('indicator-graphic2', 'figure'),
+    [dash.dependencies.Input('xaxis-column2', 'value'),
+     dash.dependencies.Input('yaxis-column2', 'value')])
+def update_graph(xaxis_column_name2, yaxis_column_name2):
+    
+    return {
+        'data': [go.Scatter(
+            x=df['TIME'].unique(),
+            y=df.set_index(['GEO', 'NA_ITEM']).loc[(xaxis_column_name2,yaxis_column_name2), 'Value'],
+            mode='lines'
+        )],
+        'layout': go.Layout(
+            xaxis={
+                'title': 'Years',
+                'type': 'linear'
+            },
+            yaxis={
+                'title': yaxis_column_name2,
+                'type': 'linear'
+            },
+            margin={'l': 40, 'b': 40, 't': 10, 'r': 0},
+            hovermode='closest'
+        )
+    }
+
+if __name__ == '__main__':
+    app.run_server()
+
